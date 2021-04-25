b7d8c7384da54fd17e21bb299081042d137bb2db
Anthony Chao <qoosuperman@gmail.com>
2021-04-25 14:32:36 +0800

    week2 hw3

--- homeworks/week2/hw3.js
+++ homeworks/week2/hw3.js
@@ -1,5 +1,10 @@
 function reverse(str) {
-  
+  var strArr = str.split('')
+  var result = []
+  for(var i =0;i<strArr.length;i++){
+    result.unshift(strArr[i])
+  }
+  console.log(result.join(''))
 }
 
 reverse('hello');
