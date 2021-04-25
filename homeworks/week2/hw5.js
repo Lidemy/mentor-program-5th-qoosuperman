e8d1079648d7336e59d2bf513287d7e7fe8c9942
Anthony Chao <qoosuperman@gmail.com>
2021-04-25 14:42:54 +0800

    week2 hw5

--- homeworks/week2/hw5.js
+++ homeworks/week2/hw5.js
@@ -1,9 +1,20 @@
 function join(arr, concatStr) {
-  
+  var result = ''
+  for(var i=0;i<arr.length;i++){
+    result += arr[i]
+    if(i !== arr.length - 1) {
+      result += concatStr
+    }
+  }
+  return result
 }
 
 function repeat(str, times) {
-  
+  var result = ''
+  for(var i=1;i<=times;i++){
+    result += str
+  }
+  return result
 }
 
 console.log(join(['a'], '!'));
